bdf3af7be049d2e9266d71f307d26afe0d5e1543
kinyeramos2021 <kinyeramo@gmail.com>
2021-08-16 10:51:03 +0300

    working on controllers,homepages and footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,32 @@
+<footer class="bg-gray-800 py-20 mt-20">
+  <div class="sm:grid grid-cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+    <div>
+      <h3 class="text-l sm:font-bold text-gray-100">
+        Pages
+      </h3>
+
+      <ul class="py-4 sm:text-s pt-4 text-gray-400">
+        <li class="pb-1">
+          <a href="/">
+            Home
+          </a>
+        </li>
+        <li class="pb-1">
+          <a href="/blog">
+            Blog
+          </a>
+        </li>
+        <li class="pb-1">
+          <a href="/login">
+            Home
+          </a>
+        </li>
+        <li class="pb-1">
+          <a href="/register">
+            Register
+          </a>
+        </li> 
+      </ul>
+    </div>
+  </div>
+</footer>
